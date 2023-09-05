ea92c767ab9f0f8464aff25b9eedca6628f27b53
DariaTishina <47531362+DariaTishina@users.noreply.github.com>
2023-09-05 18:14:46 +0300

    Update lab1task2.py

--- lab1task2.py
+++ lab1task2.py
@@ -1,12 +1,15 @@
+#Создание функции для сравнения первых/последних чисел
 def task2(x,y):
     if x[0] == y[0] or x[len(x)-1] == y[len(y)-1]:
         return True
     else:
         return False
-        
+
+#Создание пустых списков
 x=[]
 y=[]
 
+#Заполнение первого списка
 print ('Заполните первый список числами через enter, затем введите stop')
 while True: 
     el = input ()
@@ -14,7 +17,8 @@ while True:
         break
     x.append(el)
     el = None
-    
+
+#Заполнение второго списка
 print ('Заполните второй список числами через enter, затем введите stop')
 while True: 
     el = input ()
@@ -22,5 +26,6 @@ while True:
         break
     y.append(el)
     el = None
-    
+
+#Вывод результата - True или False
 print (task2(x,y))
