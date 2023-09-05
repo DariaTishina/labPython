aabc9b535afc5a4f1b46b761c8e667557d9ad0e0
DariaTishina <47531362+DariaTishina@users.noreply.github.com>
2023-09-05 18:12:36 +0300

    Update lab1task2.py

--- lab1task2.py
+++ lab1task2.py
@@ -6,14 +6,14 @@ def task2(x,y):
 
 x=[]
 y=[]
-print ('Заполните первый список числами, затем введите stop')
+print ('Заполните первый список числами через enter, затем введите stop')
 while True: 
     el = input ()
     if el == 'stop':
         break
     x.append(el)
     el = None
-print ('Заполните второй список числами, затем введите stop')
+print ('Заполните второй список числами через enter, затем введите stop')
 while True: 
     el = input ()
     if el == 'stop':
