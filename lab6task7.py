3d448acad73910f06c20f6ec4f5fe6eed8276126
DariaTishina <47531362+DariaTishina@users.noreply.github.com>
2023-10-01 13:42:21 +0300

    Create lab6task7.py

---
+++ lab6task7.py
@@ -0,0 +1,18 @@
+import psycopg2
+import json
+
+def lab6task7():
+
+    # параметры соединения с БД
+    connection = psycopg2.connect(dbname="postgres",user="postgres",password="1",host="127.0.0.1",port="5432")
+    cursor = connection.cursor()
+    # запрос для выбора всех таблиц схемы
+    tables ="""SELECT * FROM pg_catalog.pg_tables WHERE schemaname = 'public';"""
+    # выполнение запроса
+    cursor.execute(tables)
+    # возврат в виде списка
+    results = cursor.fetchall()
+    with open('result.txt', 'a') as file:
+        json.dump(results, file)
+
+lab6task7()
